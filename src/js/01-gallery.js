141e5e0605b21d6b4c7104d756b2e32324f4a822
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-10-29 20:54:18 +0300

    21358

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+// import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRefs = document.querySelector('.gallery');
+const galleryItemsRefs = galleryItemsMarkup(galleryItems);
+
+galleryRefs.innerHTML = galleryItemsRefs;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function galleryItemsMarkup(items) {
+  return items
+    .map(
+      ({ original, preview, description } = items) =>
+        `<a class="gallery__item" href="${original}"><img class="gallery__image"src="${preview}" alt="${description}" /></a> `
+    )
+    .join('');
+}
